312c2064e718a60b7188b132da67cca09f353d08
Alireza Rahimi <ali938414@gmail.com>
2020-02-07 13:35:35 +0330

    add map and some button add some bugs

--- app/src/main/java/me/coleo/snapion/Activities/MainActivity.java
+++ app/src/main/java/me/coleo/snapion/Activities/MainActivity.java
@@ -3,15 +3,27 @@ package me.coleo.snapion.Activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.neshan.core.LngLat;
+import org.neshan.services.NeshanMapStyle;
+import org.neshan.services.NeshanServices;
+import org.neshan.ui.MapView;
+
 import me.coleo.snapion.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        map = findViewById(R.id.map);
+        LngLat focalPoint = new LngLat(59.6168, 36.2605);
+        map.setFocalPointPosition(focalPoint, 0f);
+        map.setZoom(14f, 0.5f);
+        map.getLayers().add(NeshanServices.createBaseMap(NeshanMapStyle.STANDARD_DAY));
     }
 
 }
